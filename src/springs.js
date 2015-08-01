c135e2670a93c79a31a4603dd062aae28f7364cf
John Sarracino <jsarraci@cs.ucsd.edu>
2015-08-01 15:12:23 -0700

    first draft of spring constraints, bigger canvas

--- src/springs.js
+++ src/springs.js
@@ -2,14 +2,36 @@ function init() {
   all_objects = [];
   drag_points = [];
 
-  S1 = Spring(200, 200, 0, -100, "black");
-  I1 = InteractionPoint (200, 200);
-  I2 = InteractionPoint (200, 100);
 
-  T = 0;
+  S1 = Spring(200, 300, 0, -200, "black");
+  I1 = InteractionPoint (S1.x, S1.y);
+  I2 = InteractionPoint (S1.x, S1.y+S1.dy);
+
+  RestLengths = {s1: 125};
+  G = 9.8;
+  Ks = {s1: 5};
+  Masses = {s1: 2000};
+  Dampers = {s1: 10};
+  Initials = {s1: {v: 0, dy: S1.dy, dx: S1.dx}};
+  CurrentVs = {s1: 0};
 
   push(all_objects, S1, I1, I2);
   push(drag_points, I1, I2);
+
+
+  // initialize timer
+
+  tau = Timer(10, function(t) {
+    update_constraints();
+    global_redraw();
+  }, function() {
+    CurrentVs.s1 = Initials.s1.v;
+    S1.dy = Initials.s1.dy;
+    S1.dx = Initials.s1.dx;
+    I2.y = S1.y + S1.dy;
+    I2.x = S1.x + S1.dx;
+    global_redraw();
+  });
 }
 
 function drag_update() {
@@ -18,8 +40,24 @@ function drag_update() {
 
   S1.dx = (I2.x - S1.x);
   S1.dy = (I2.y - S1.y);
+
 }
 
 function update_constraints() {
+  // F = kx - cv = ma => dv = (kx-cv)/m
+  CurrentVs.s1 = CurrentVs.s1 + (Ks.s1*(-1*(S1.dy + RestLengths.s1)) - Dampers.s1*CurrentVs.s1)/ Masses.s1;
+  // dx/dt = v => dx = v
+  S1.dy = S1.dy + CurrentVs.s1;
 
+  I2.y = S1.y + S1.dy;
+}
+
+function start() {
+  tau.start();
+}
+function stop() {
+  tau.stop();
+}
+function reset() {
+  tau.reset();
 }
