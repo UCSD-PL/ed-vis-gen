71f2336d17cb05c0951bffe77a521994ddb93bd9
Noah Marcus <nmarcus@hmc.edu>
2015-08-15 10:41:49 -0700

    Pig updated, interactive platform height

--- src/mechanics.js
+++ src/mechanics.js
@@ -2,15 +2,20 @@ function init() {
   all_objects = [];  
   drag_points = [];
   rightClick_points = [];
+  inc_objects = [];
 
   PigVelocity = {x:0, y:0};
   Initial ={ramp: {x1:100 , y1:163 , x2:100 , y2:250 , x3:250, y3:250},
             v: {x:0, y:0}, 
             mu: {mn:0, mx:1, val:.55},
             g: {mn:0, mx:2, val:1},
-            ang: {mn:0, mx:Math.PI/2, val:Math.PI/6},
-            pig: {x:175, y:207},
-            mass: {mn:10000, mx:20000, val:15000}
+            ang: {val:Math.PI/6},
+            pig: {x:100, y:140},
+            mass: {mn:100, mx:200, val:150},
+            fricvec: {x: 100, y: 140, dx: -51, dy: -51},
+            normvec: {x: 100, y: 140, dx: 92, dy: -92},
+            gravvec: {x: 100, y: 140, dx: 0, dy: +150},
+            i1: {x:100, y:163}
             };
   
   Ramp = Triangle (
@@ -26,9 +31,6 @@ function init() {
   addSlider("Mass", "sliders", Initial.mass.mn, Initial.mass.mx, Initial.mass.val, function(o) {
       CurrConstants.mass = getSliderValue("Mass");
   });
-  addSlider("Angle", "sliders", Initial.ang.mn, Initial.ang.mx, Initial.ang.val, function(o) {
-      CurrConstants.ang = getSliderValue("Angle");
-  });
   addSlider("Friction", "sliders", Initial.mu.mn, Initial.mu.mx, Initial.mu.val, function(o) {
       CurrConstants.mu = getSliderValue("Friction");
   });
@@ -36,22 +38,17 @@ function init() {
       CurrConstants.g = getSliderValue("Gravity");
   });
   
-   /*
-  FricVec = Arrow (Initial.x, Initial.y, -50, -50, "red");
+  Height = InteractionPoint(Initial.i1.x, Initial.i1.y);
   
-  NormVec = Arrow (Initial.x, Initial.y, 50, -50, "red");
+  FricVec = Arrow (Initial.fricvec.x, Initial.fricvec.y, Initial.fricvec.dx, Initial.fricvec.dy, "red");
   
-  GravVec = Arrow (Initial.x, Initial.y, 0, +150, "red");
-  */
+  NormVec = Arrow (Initial.normvec.x, Initial.normvec.y, Initial.normvec.dx, Initial.normvec.dy, "red");
+  
+  GravVec = Arrow (Initial.gravvec.x, Initial.gravvec.y, Initial.gravvec.dx, Initial.gravvec.dy, "red");
+  
 
   Title = Text(50, 50, "Pig on a ramp with friction", "16pt Comic sans MS");
 
-  // right now, start/stop/reset are handled as html buttons
-  // TODO: implement these
-  //Start = TextCircle(400, 100, 40, -15, 10, "Go", "black");
-  //SI = InteractionPoint(400, 100);
-  //Reset = TextCircle(400, 200, 40, -30, 10, "Reset", "black");
-  //RI = InteractionPoint(400, 200);
 
   T = 0;
 
@@ -64,18 +61,16 @@ function init() {
     restore(Pig, Initial.pig);
     restore(PigVelocity, Initial.v);
     restore(Ramp, Initial.ramp)
-    /*
-    FricVec.dx = -50;
-    FricVec.dy = -50;
-    NormVec.dx = 50;
-    NormVec.dy = -50;
-    GravVec.dx = 0;
-    GravVec.dy = 150;
-    */
+    restoreAll([
+      // Vectors
+      NormVec, Initial.normvec,
+      FricVec, Initial.fricvec,
+      GravVec, Initial.gravvec,
+      Height, Initial.i1
+      ]);
 
     setSliderValue("Mass", Initial.mass.val);
     setSliderValue("Friction", Initial.mu.val);
-    setSliderValue("Angle", Initial.ang.val);
     setSliderValue("Gravity", Initial.g.val);
 
     for (var e in CurrConstants) {
@@ -86,14 +81,31 @@ function init() {
     global_redraw();
   }); // executes every 10ms (.01s)
 
-  push(all_objects, /*FricVec, NormVec, GravVec,*/ Title, Pig, Ramp /*,Start, Reset*/);
-  //push(drag_points, I1, I2, I3 /*,SI, RI*/);
+  push(all_objects, FricVec, NormVec, GravVec, Title, Pig, Ramp, Height);
+  push(drag_points, Height);
 
 
 }
 
 function drag_update() {
+  Height.x = Initial.i1.x;
   
+  var lastRamp = Ramp.y1;
+  Ramp.y1 = Height.y;
+  CurrConstants.ang = Math.atan((Ramp.y2-Ramp.y1)/(Ramp.x2-Ramp.x3));
+  
+
+  // Get pig to drop the correct amount
+  // non-trivial geometry problem
+  var bigHeight = Ramp.y2 - lastRamp;
+  var bigDelta = Ramp.y1 - lastRamp;
+  var smallHeight = Ramp.y2 - Pig.y;
+  var smallDelta = smallHeight*bigDelta/bigHeight;
+  Pig.y += bigDelta;
+
+  FricVec.x = GravVec.x = NormVec.x = Pig.x;
+  FricVec.y = GravVec.y = NormVec.y = Pig.y;
+
 }
 
 function rightClick_update() {
@@ -106,6 +118,12 @@ function update_constraints() {
   Grav = CurrConstants.mass*G;
   NormF = Grav*Math.cos(CurrConstants.ang);
   Fric = CurrConstants.mu*NormF;
+
+  // calculating appropriate vector lengths
+  FricVec.dx = FricVec.dy = -.707*Fric/10;
+  GravVec.dy = +Grav/10;
+  NormVec.dy = -(NormVec.dx = NormF*.707/10)
+
   
   netForce = Math.max(0, Grav*Math.sin(CurrConstants.ang)-Fric);
 
@@ -114,7 +132,7 @@ function update_constraints() {
   // v = dx/dt => dx = v
   Pig.x = Pig.x + PigVelocity.x;
   Pig.y = Pig.y + PigVelocity.y;
-  /*
+  
   // update the vector positions
   FricVec.x = Pig.x;
   FricVec.y = Pig.y;
@@ -124,11 +142,9 @@ function update_constraints() {
 
   GravVec.x = Pig.x;
   GravVec.y = Pig.y;
-  */
-
-  // TODO - make ramp height actually change, possibly with interaction
-  //      - show force vectors with arrows
-}
+  
+  if (Pig.x > Ramp.x3) {tau.stop();}
+  }
 
 function start() {
   tau.start();
