57116f056e5587657818490847b067751dd14ffa
John Sarracino <jsarraci@cs.ucsd.edu>
2015-09-02 15:25:53 -0700

    lowlevel emitting full programs

--- src/backend/scala/Emit.scala
+++ src/backend/scala/Emit.scala
@@ -35,7 +35,7 @@ object Allocator {
   object Variables extends Inner[Variable]
   object IPoints extends Inner[IPoint]
   object Shapes extends Inner[Shape]
-  def apply[T: VorIPorShp](v: T) = v match {
+  def apply[T: VorIPorShp](v: T): String = v match {
     case v@Variable(_) ⇒ Variables(v)
     case v@IPoint(_, _, _) ⇒ IPoints(v)
     case v: Shape ⇒ Shapes(v)
@@ -117,19 +117,35 @@ object HighLevel extends Emitter {
 // print out a low-level javascript version of the program
 object LowLevel extends Emitter {
   override def TODO = "//@TODO"
+  def initPreamble =
+    vsep(Seq("all_objects", "drag_points", "inc_objects").map(s ⇒ text(s ++ " = [];")))
+  def timestep = text("20")
+  def initEpilogue = {
+    val timerBody = emitFCall("update_constraints", Seq()) <@> emitFCall("global_redraw", Seq())
+    "tau =" <+> printConstructor("Timer", Seq(timestep, // update frequency
+      emitFunc("", Seq(text("t")), timerBody), // onTick
+      emitFunc("", Seq(), timerBody))) // onReset
+  }
+  // for some reason, the library doesn't define it...
+  def sep(ds: Seq[Doc], sep: Doc) = group(vsep(ds, sep))
+
   def varPreamble = "//VARIABLES:"
   def ipPreamble = "//IPOINTS:"
   def shpPreamble = "//SHAPES:"
   def eqPreamble = "init_stays(); // SUPER IMPORTANT NEED THIS CALL" <@> "//EQUATIONS:"
 
   def emitFunc(name: String, args: Seq[Doc], body: Doc): Doc = {
-    "function" <+> name <+> parens(fillsep(args, ",")) <+> nest(braces(
-      line <> body <> line))
+    "function" <+> name <+> parens(fillsep(args, ",")) <+> "{" <@>
+      nest(indent(body)) <@> "}"
+  }
+
+  def emitFCall(name: String, args: Seq[Doc]) = {
+    name <> parens(nest(sep(args, ","))) <> semi
   }
 
   def printVar(v: Variable, σ:Store) =  {
-    text(v.name) <+> "=" <+> "makeVariable" <> parens(
-      dquotes(text(v.name)) <> comma <+> σ(v).toString) <> semi
+    text(v.name) <+> "=" <+>
+      emitFCall("makeVariable", Seq(dquotes(text(v.name)), text(σ(v).toString)))
   }
   def printShape(s: Shape) = Allocator(s) <+> "=" <+> {
     val (ctor:String, symbArgs) = s match {
@@ -162,11 +178,9 @@ object LowLevel extends Emitter {
   }
 
   def printEquation(e: Eq) = {
-    "addEquation" <> parens (
-      nest( line <> vsep(
-        Seq(printExpr(e.lhs), printExpr(e.rhs)), comma
-      )) <> line
-    ) <> semi
+    emitFCall("addEquation", Seq(
+      printExpr(e.lhs), printExpr(e.rhs)
+    ))
   }
 
   def printExpr(e: Expr) = e match { case Expr(c, vars) ⇒ {
@@ -203,7 +217,7 @@ object LowLevel extends Emitter {
       case LineSegment(Point(a,b), Point(c,d)) ⇒
         Seq(("x1", a), ("y1", b), ("x2", c), ("y2", d))
       case Rectangle(Point(x,y), h, w) ⇒
-        Seq(("x1", x), ("y1", y), ("x2", h), ("y2", w)) // again, ugh...
+        Seq(("x1", x), ("y1", y), ("x2", w), ("y2", h)) // again, ugh...
       case Circle(Point(x,y), r) ⇒ Seq(("x", x), ("y", y), ("r", r))
       case Triangle(Point(a,b), Point(c,d), Point(e,f)) ⇒
         Seq(("x1", a), ("y1", b), ("x2", c), ("y2", d), ("x3", e), ("y3", f))
@@ -236,12 +250,42 @@ object LowLevel extends Emitter {
     )
   }
 
-  override def emitProg(p: Program, σ: Store): Doc = {
-    vsep(
-      Seq(super.emitProg(p, σ),
-      "// update_constraints",
-      emitFunc("update_constraints", Seq(), emitDrawUpdates(p.shapes)))
+  def emitInit(body: Doc): Doc = {
+    emitFunc("init", Seq(), vsep(Seq(
+      initPreamble, body, initEpilogue))
     )
+  }
+
+  override def emitProg(p: Program, σ: Store): Doc = {
+    // vsep(
+    //   Seq(super.emitProg(p, σ),
+    //   "// update_constraints",
+    //   emitFunc("update_constraints", Seq(), emitDrawUpdates(p.shapes)))
+    // )
+
+    // emit init
+    // rely on superclass for most of the gruntwork. in addition to variable declarations,
+    // foreach shape + ip, we need to emit a call to push(all_objects, s)
+    // and foreach ip, we need to emit a call to push(drag_objects, ip)
+    val ipoints = p.ipoints.map(Allocator(_))
+    val objects = Seq("all_objects") ++ p.shapes.map(Allocator(_)) ++ ipoints
+    val initBody =
+      super.emitProg(p, σ) <@>
+      emitFCall("push", objects.map(text(_))) <@>
+      emitFCall("push", (Seq("drag_points") ++ ipoints).map(text _))
+
+    vsep(Seq(
+      emitInit(initBody),
+      // emit update_constraints
+      emitFunc("update_constraints", Seq(), emitDrawUpdates(p.shapes)),
+      // emit remaining stubs
+      emitFunc("drag_update", Seq(), emitFCall("update_constraints", Seq())),
+      emitFunc("start", Seq(), text("tau.start();")),
+      emitFunc("stop", Seq(), text("tau.stop();")),
+      emitFunc("reset", Seq(), text("tau.reset();")),
+      emitFunc("on_release", Seq(), empty),
+      emitFunc("on_click", Seq(), empty)
+    ))
 
   }
 }
--- src/frontend/eddie/simple.js
+++ src/frontend/eddie/simple.js
@@ -1,71 +1,85 @@
-function init() {
-  all_objects = [];
-  drag_points = [];
-  inc_objects = [];
-
-  //VARIABLES:
-      PY = makeVariable("PY", 50.0);
-      PYPH = makeVariable("PYPH", 70.0);
-      W = makeVariable("W", 10.0);
-      PX = makeVariable("PX", 50.0);
-      PXMW = makeVariable("PXMW", 40.0);
-      H = makeVariable("H", 20.0);
-  //IPOINTS:
-      IP54 = InteractionPoint( PXMW, PYPH );
-      IP54.links = ["PXMW", "PYPH", "PX", "H"];
-  //SHAPES:
-      S0 = Rectangle( PX.value-W.value, PY.value-H.value, W.value+PX.value, H.value+PY.value,
-          "black", "rgba(0,0,0,0)" );
-  init_stays(); // SUPER IMPORTANT NEED THIS CALL
-  //EQUATIONS:
-      addEquation(    fromConst(0.0).plus(fromVar(PXMW).times(1.0)),
-          fromConst(0.0).plus(fromVar(PX).times(1.0).plus(fromVar(W).times(-1.0))));
-      addEquation(    fromConst(0.0).plus(fromVar(PYPH).times(1.0)),
-                      fromConst(0.0).plus(fromVar(H).times(1.0).plus(fromVar(PY).times(1.0))));
-  push(all_objects, IP54, S0);
-  push(drag_points, IP54);
-
-  // initialize timer
-
-  tau = Timer(20, function(t) {
+function init () {
+    all_objects = [];
+    drag_points = [];
+    inc_objects = [];
+    //VARIABLES:
+    CY = makeVariable("CY", 50.0);
+    CX = makeVariable("CX", 50.0);
+    RXPRW = makeVariable("RXPRW", 120.0);
+    R = makeVariable("R", 20.0);
+    RYMRH = makeVariable("RYMRH", 70.0);
+    RH = makeVariable("RH", 30.0);
+    RY = makeVariable("RY", 100.0);
+    RX = makeVariable("RX", 100.0);
+    RW = makeVariable("RW", 20.0);
+    //IPOINTS:
+    IP77 = InteractionPoint(
+        RXPRW,
+        RYMRH
+    );
+    IP77.links = ["CY", "CX", "RXPRW", "RYMRH", "RY", "RX"];
+    //SHAPES:
+    S0 = Circle(
+        CX.value,
+        CY.value,
+        R.value,
+        "black",
+        "rgba(0,0,0,0)"
+    );
+    S1 = Rectangle(
+        RX.value-RW.value,
+        RY.value-RH.value,
+        RW.value+RX.value,
+        RH.value+RY.value,
+        "black",
+        "rgba(0,0,0,0)"
+    );
+    init_stays(); // SUPER IMPORTANT NEED THIS CALL
+    //EQUATIONS:
+    addEquation(fromConst(0.0).plus(fromVar(CX).times(1.0)),
+        fromConst(-50.0).plus(fromVar(RX).times(1.0)));
+    addEquation(fromConst(0.0).plus(fromVar(CY).times(1.0)),
+        fromConst(-50.0).plus(fromVar(RY).times(1.0)));
+    addEquation(fromConst(0.0).plus(fromVar(RXPRW).times(1.0)),
+        fromConst(0.0).plus(fromVar(RW).times(1.0).plus(fromVar(RX).times(1.0))));
+    addEquation(fromConst(0.0).plus(fromVar(RYMRH).times(1.0)),
+        fromConst(0.0).plus(fromVar(RY).times(1.0).plus(fromVar(RH).times(-1.0))));
+    push(all_objects, S0, S1, IP77);
+    push(drag_points, IP77);
+    tau = Timer(
+        20,
+        function  (t) {
+            update_constraints();
+            global_redraw();
+        },
+        function  () {
+            update_constraints();
+            global_redraw();
+        }
+    );
+}
+function update_constraints () {
+    S0.x = CX.value; S0.y = CY.value; S0.r = R.value;
+    S1.x1 = RX.value-RW.value;
+    S1.y1 = RY.value-RH.value;
+    S1.x2 = RW.value+RX.value;
+    S1.y2 = RH.value+RY.value;
+}
+function drag_update () {
     update_constraints();
-    global_redraw();
-  }, function() {
-    update_constraints();
-    global_redraw();
-  });
 }
-
-function drag_update() {
-  update_constraints();
+function start () {
+    tau.start();
 }
-
-function update_constraints() {
-
-  // rectangle
-  S0.x1 = PX.value - W.value;
-  S0.y1 = PY.value - H.value;
-  S0.x2 = PX.value + W.value;
-  S0.y2 = PY.value + H.value;
-  // circle
-  // S0.x = PX.value;
-  // S0.y = PY.value;
-  // S0.r = R.value;
+function stop () {
+    tau.stop();
+}
+function reset () {
+    tau.reset();
+}
+function on_release () {
 
 }
+function on_click () {
 
-function start() {
-  tau.start();
-}
-function stop() {
-  tau.stop();
-}
-function reset() {
-  tau.reset();
-}
-
-function on_release() {
-}
-
-function on_click() {
 }
