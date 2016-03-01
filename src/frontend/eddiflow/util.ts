e517890efa40476b86dcc1a1fe66cd93262dba5b
John Sarracino <jsarraci@cs.ucsd.edu>
2016-02-29 17:43:18 -0800

    added debugging output, got a simple test to work

--- src/frontend/eddiflow/util.ts
+++ src/frontend/eddiflow/util.ts
@@ -1,68 +1,70 @@
 
 
+/// <reference path="../../../typings/browser/ambient/underscore/underscore.d.ts" />
+import _ = require('underscore');
 
+export interface Map<T> {
+  [K: string]: T;
+}
 
-module Eddiflow {
-  export interface Map<T> {
-    [K: string]: T;
+export function filterMap<T> (m: Map<T>, filtee: (k: string, v: T) => boolean): Map<T> {
+  let ret: Map<T> = {};
+  for (var key in m) {
+    if (filtee(key, m[key]))
+      ret[key] = m[key];
   }
 
-  export function filterMap<T> (m: Map<T>, filtee: (k: string, v: T) => boolean): Map<T> {
-    let ret: Map<T> = {};
-    for (var key in m) {
-      if (filtee(key, m[key]))
-        ret[key] = m[key];
-    }
-
-    return ret;
+  return ret;
+}
+export class Unimplemented extends Error {
+  constructor(message: string) {
+    super();
+    this.message = message;
+    this.name = "unimplemented";
   }
-  export class Unimplemented extends Error {
-    constructor(message: string) {
-      this.message = message;
-      this.name = "unimplemented";
-      super();
-    }
+}
+
+export class BadExprFormat extends Error {
+  constructor(message: string) {
+    super();
+    this.message = message;
+    this.name = "Bad Expression Format";
+  }
+}
+
+export class PatternMatchFailed extends Error {
+  constructor(message: string) {
+    super();
+    this.message = message;
+    this.name = "Inexaustive pattern match";
+  }
+}
+
+export function eq(l: any, r: any): boolean {
+  return l == r;
+}
+
+export function match<matchT, retT, errT extends Error>(
+  obj: matchT,
+  matcher: (l: matchT, r: matchT) => boolean,
+  cases: [matchT, retT][],
+  notFoundError?: errT
+): retT {
+  let res: [matchT, retT] = _.find(cases, (tup:[matchT, retT]) => {
+    //console.log("matching " + tup[0].toString() + " vs " + obj.toString());
+    return matcher(obj, tup[0]);
+  });
+
+  if (res !== undefined) {
+    return res[1];
   }
 
-  export class BadExprFormat extends Error {
-    constructor(message: string) {
-      this.message = message;
-      this.name = "Bad Expression Format";
-      super();
-    }
-  }
+  // typescript has default argument types, but getting one to work for an alloc'd expr
+  // is beyond my wizardry
 
-  class PatternMatchFailed extends Error {
-    constructor(message: string) {
-      this.message = message;
-      this.name = "Inexaustive pattern match";
-      super();
-    }
-  }
-
-  export function match<matchT, retT, errT extends Error>(
-    obj: matchT,
-    matcher: (l: matchT, r: matchT) => boolean,
-    cases: [matchT, retT][],
-    notFoundError?: errT
-  ): retT {
-    let res: [matchT, retT] = _.find(cases, (tup:[matchT, retT]) => {
-      console.log("matching " + tup[0].toString() + " vs " + obj.toString());
-      return matcher(obj, tup[0]);
-    });
-
-    if (res !== undefined) {
-      return res[1];
-    }
-
-    // typescript has default argument types, but getting one to work for an alloc'd expr
-    // is beyond my wizardry
-
-    if (!notFoundError)
-      throw new PatternMatchFailed("can't match " + obj.toString() + " to " + cases.toString());
-    else
-      throw notFoundError;
-
-  }
+  if (!notFoundError)
+    throw new PatternMatchFailed("can't match " + obj.toString() + " to " + cases.toString());
+  else
+    throw notFoundError;
 
 }
