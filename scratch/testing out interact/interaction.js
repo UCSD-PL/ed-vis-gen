1d4a9467a3ea84d3b80703dc354aed2d4a8f4a92
Odaris <i.am.odaris@gmail.com>
2016-06-26 20:08:47 -0700

    Interaction.js file is still broken because I'm trying to incporate snapping. Still a failure. :(

--- scratch/testing out interact/interaction.js
+++ scratch/testing out interact/interaction.js
@@ -1,49 +1,55 @@
-// target elements with the "draggable" class
-interact('.drag-and-resize')
+interact('.resize-drag')
   .draggable({
-    onmove: window.dragMoveListener,
-    // call this function on every dragend event
+    snap: {
+      targets: [
+        interact.createSnapGrid({ x: 15, y: 15 })
+      ]
+      range: Infinity
+    },
+    onmove: dragMoveListener
   })
+
   .resizable({
-        preserveAspectRatio: true,
-        edges: { left: true, right: true, bottom: true, top: true}
-    });
-
-  function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  };
-
-  // this is used later in the resizing and gesture demos
-  window.dragMoveListener = dragMoveListener;
+    preserveAspectRatio: true,
+    edges: { left: true, right: true, bottom: true, top: true }
+  })
 
   .on('resizemove', function (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0),
+        y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    // target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+  })
+
+  function dragMoveListener (event) {
    var target = event.target,
-       x = (parseFloat(target.getAttribute('data-x')) || 0),
-       y = (parseFloat(target.getAttribute('data-y')) || 0);
+       // keep the dragged position in the data-x/data-y attributes
+       x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+       y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-   // update the element's style
-   target.style.width  = event.rect.width + 'px';
-   target.style.height = event.rect.height + 'px';
+   // translate the element
+   target.style.webkitTransform =
+   target.style.transform =
+     'translate(' + x + 'px, ' + y + 'px)';
 
-   // translate when resizing from top or left edges
-   x += event.deltaRect.left;
-   y += event.deltaRect.top;
-
-   target.style.webkitTransform = target.style.transform =
-       'translate(' + x + 'px,' + y + 'px)';
-
+   // update the posiion attributes
    target.setAttribute('data-x', x);
    target.setAttribute('data-y', y);
- });
+ }
+
+ // this is used later in the resizing and gesture demos
+ window.dragMoveListener = dragMoveListener;
