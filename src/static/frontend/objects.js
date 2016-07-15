134f7ee58270a5a4761f4b8dd4b57f5d68db40ab
John Sarracino <jsarraci@cs.ucsd.edu>
2016-07-15 13:48:04 -0700

    added name allocator, fixed clean command

--- src/static/frontend/objects.js
+++ src/static/frontend/objects.js
@@ -1,27 +1,31 @@
 //Add line
 function addLine(){
- var line0 = new fabric.Line([50,100,50,300], {stroke:'cornflowerblue', strokeWidth: 2, top:100, left:100, lockRotation: true});
+ var name = allocSName();
+ var line0 = new fabric.Line([50,100,50,300], {name: name, stroke:'cornflowerblue', strokeWidth: 2, top:100, left:100, lockRotation: true});
  canvas.add(line0);
  updateLog();
 }
 
 //Add triangle
 function addTriangle(){
-  var triangle0 = new fabric.Triangle({ width: 30, height: 30, fill:'cornflowerblue', top:100, left:100, lockRotation: true});
+  var name = allocSName();
+  var triangle0 = new fabric.Triangle({ name: name, width: 30, height: 30, fill:'cornflowerblue', top:100, left:100, lockRotation: true});
   canvas.add(triangle0);
   updateLog();
 }
 
 //Add Circle
 function addCircle(){
-  var circle0 = new fabric.Circle({ radius: 30, fill: 'dodgerblue', top: 100, left: 100, lockRotation: true});
+  var name = allocSName();
+  var circle0 = new fabric.Circle({ name: name, radius: 30, fill: 'dodgerblue', top: 100, left: 100, lockRotation: true});
   canvas.add(circle0);
   updateLog();
 }
 
 //Add rectangle
 function addRectangle(){
-  var rectangle0 = new fabric.Rect({ width: 30, height:30, fill:'royalblue', top: 100, left:100, lockRotation: true});
+  var name = allocSName();
+  var rectangle0 = new fabric.Rect({ name: name, width: 30, height:30, fill:'royalblue', top: 100, left:100, lockRotation: true});
   canvas.add(rectangle0);
   updateLog();
 }
@@ -62,6 +66,13 @@ function EnterURL(){
       canvas.add(img);
   });}}
 
+// allocator for names
+var allocSName = (function() {
+  var suffix = 0;
+  return function() {
+    return "S" + (suffix++).toString()
+  };
+})()
 
 function transmit() {
   var fabricJSON = JSON.parse(canvas);
