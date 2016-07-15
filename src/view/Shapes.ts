a48eb2580b4320a1a6c401289dc758163961a5a5
John Sarracino <jsarraci@cs.ucsd.edu>
2016-07-15 15:32:13 -0700

    fix to shape rendering

--- src/view/Shapes.ts
+++ src/view/Shapes.ts
@@ -51,7 +51,7 @@ function _drawLine(
   close = close || false
   stroke = stroke || "black"
   fill = fill || "rgba(0,0,0,0)"
-  dash = dash || true
+  dash = dash || false
 
   ctx.save()
   let len = points.length
@@ -110,7 +110,7 @@ function drawLine(ctx: Context, line: S.Line, store: Env) {
 
 function drawCircle (ctx: Context, circ: S.Circle, store: Env) {
   let [x, y, r] = U.map3Tup([circ.x, circ.y, circ.r], i => store.get(i))
-  _drawCircle(ctx, x, y, r, circ.stroke, "green")
+  _drawCircle(ctx, x, y, r, circ.stroke, "rgba(0,0,0,0)")
 }
 
 function drawImage(ctx: Context, i: S.Image, src: HTMLImageElement, store: Env) {
