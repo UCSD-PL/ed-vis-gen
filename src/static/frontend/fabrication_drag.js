a81901b3295ebc573dfb5826f7d49a9831c1a243
Odaris Barrios-Arciga <i.am.odaris@gmail.com>
2016-07-20 13:53:45 -0700

    Merged the changes onto index.html

--- src/static/frontend/fabrication_drag.js
+++ src/static/frontend/fabrication_drag.js
@@ -140,7 +140,8 @@ function candidatePoints() {
 
         else {
           this.set({
-            fill: 'black'
+            fill: 'black',
+            choice: 0
           });
 
           function checkDP (dp) {
