6f5ac39cb92441534959107a36f03d6dff408fc9
Odaris Barrios-Arciga <i.am.odaris@gmail.com>
2016-08-17 18:11:00 -0700

    Started making improvements on the snapping.

--- src/static/frontend/snapping.js
+++ src/static/frontend/snapping.js
@@ -1,18 +1,105 @@
 /////////
 /// snapping
-/// credits to futurist @http://fiddle.jshell.net/futurist/cjLg9kw7/
+/// credits to Anna Phillips @https://jsfiddle.net/aphillips8/31qbr0vn/1/
 /////////
 
 var snapping = true, // true permits snapping on the "canvas" canvas
+newArray = [],
 snap = 8; // pixels to snap;
 
-function snapToNearestCoordinate(obj, target) {
-	obj.on('moving', function () {})
+function alignContactPoints(obj, target, array) {
+	if (array.length != 0) {
+		var dx = array[0],
+		dy = array[1];
+
+		obj.set({
+			left: target.getLeft() + target.getWidth()*dx,
+			top: target.getTop() + target.getHeight()*dy
+		});
+}}
+
+function findCP(array, dx, dy) {
+	for (var i = 0; i < array.length; i++) {
+		if (dx == array[i][0] && dy == array[i][1]) {
+			return array[i];
+		}
+	}
+	return [];
+}
+
+function checkForContactPoints(obj, target, dx, dy) {
+	if (obj.get('physics') === 'pendulum') {
+    if (obj.get('item') === 'bob') {
+			newArray = findCP(arrayBob, dx, dy);
+      alignContactPoints(obj, target, newArray);
+    }
+    if (obj.get('item') === 'pivot') {
+			newArray = findCP(arrayPiv, dx, dy);
+      alignContactPoints(obj, target, newArray);
+    }
+  }
+  else if (obj.get('physics') === 'spring') {
+			newArray = findCP(arraySpring, dx, dy);
+      alignContactPoints(obj, target, newArray);
+  }
+  else if (obj.get('physics') === 'none') {
+    if (obj.get('type') === 'arrow') {
+			newArray = findCP(arrayArr, dx, dy);
+      alignContactPoints(obj, target, newArray);
+    }
+    else if (obj.get('type') === 'rect') {
+			newArray = findCP(arrayRect, dx, dy);
+      alignContactPoints(obj, target, newArray);
+    }
+    else if (obj.get('type') === 'circle') {
+			newArray = findCP(arrayCirc, dx, dy);
+      alignContactPoints(obj, target, newArray);
+    }
+    else if (obj.get('type') === 'line') {
+			newArray = findCP(arrayLine);
+      alignContactPoints(obj, target, newArray);
+    }
+    else { return; }
+  }
+  else { return; }
+}
+
+function snapToNearestContactPoint(obj, target) {
+ 	if (obj.getTop() >= target.getTop() - target.getHeight()*0.2
+	&& obj.getLeft() >= target.getLeft() + target.getWidth()) {
+		if (target.getTop() + target.getHeight()*0.25 >= obj.getTop()
+				&& obj.getTop() >= target.getTop()) {
+			checkForContactPoints(obj, target, 1, 0);
+		}
+		else if (target.getTop() + target.getHeight()*0.25 < obj.getTop()
+				&& obj.getTop() <= target.getTop() + target.getHeight()*0.75) {
+			checkForContactPoints(obj, target, 1, 0.5);
+		}
+		else if (target.getTop() + target.getHeight()*0.25 <= obj.getTop()
+				&& obj.getTop() >= target.getTop() + target.getHeight()) {
+			checkForContactPoints(obj, target, 1, 1);
+		}
+		else { }
+	}
+	else if (obj.getLeft() < target.getLeft() + target.getWidth()) {
+		if (obj.getLeft() > target.getLeft() + target.getWidth()*0.75) {
+			checkForContactPoints(obj, target, 1, 1);
+		}
+		else if (obj.getLeft() > target.getLeft() + target.getWidth()*0.25
+			&& obj.getLeft() < target.getLeft() + target.getWidth()*0.75) {
+			checkForContactPoints(obj, target, 0.5, 1);
+		}
+		else if (obj.getLeft() <= target.getLeft() + target.getWidth()*0.25) {
+			checkForContactPoints(obj, target, 0, 0);
+		}
+		else { }
+	}
+	else { }
 }
 
 function findNewPos(distX, distY, target, obj) {
 	// See whether to focus on X or Y axis
-	if(Math.abs(distX) > Math.abs(distY)) {
+	if (Math.abs(distX) > Math.abs(distY)) {
 		if (distX > 0) {
 			target.setLeft(obj.getLeft() - target.getWidth());
 		} else {
@@ -70,6 +157,7 @@ canvas.on('object:moving', function (options) {
 
 			// Set new position
 			findNewPos(distX, distY, options.target, obj);
+			snapToNearestContactPoint(options.target, obj);
 		}
 
 		// Snap objects to each other horizontally
